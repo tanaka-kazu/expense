b0a0657898fd7e62b116fc481ab6a9f23e49eede
tanaka-kazu <asterisk.tanaka@gmail.com>
2017-09-02 13:57:37 +0900

    add download csv button

--- js/generate_csv.js
+++ js/generate_csv.js
@@ -1,5 +1,22 @@
 (function() {
     "use strict";
-    alert("Call Javascript!!");
-})();
+    kintone.events.on('app.record.index.show', function(event) {
 
+        if(!/未対応/.test(event.viewName) && !/出力用/.test(event.viewName)) {
+            return;
+        }
+
+        if (document.getElementById('dl_csv_button') !== null) {
+            return;
+        }
+        var dlCsvButton = document.createElement('button');
+        dlCsvButton.id = 'dl_csv_button';
+        dlCsvButton.innerHTML = 'CSVダウンロード';
+
+        dlCsvButton.onclick = function() {
+            window.confirm('CSVをダウンロードします');
+        };
+
+        kintone.app.getHeaderMenuSpaceElement().appendChild(dlCsvButton);
+    });
+})();
